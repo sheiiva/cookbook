ef38d06aff1d121d7642fd6746e4660f7090a1d8
Corentin Coutret Rozet <corentin.crzt@gmail.com>
2025-09-13 15:11:05 +0200

    [UPDATE] - Enhance RecipeDetailViewer and RecipeLoader to support dynamic language selection; add methods for updating the URL and language display based on user selection.

--- js/recipeLoader.js
+++ js/recipeLoader.js
@@ -164,7 +164,7 @@ class RecipeLoader {
             recipesInCategory.forEach(recipe => {
                 const li = document.createElement('li');
                 const a = document.createElement('a');
-                a.href = `recipe-template.html?id=${recipe.id}`;
+                a.href = `recipe-template.html?id=${recipe.id}&lang=${this.currentLanguage}`;
                 a.textContent = recipe.title;
                 a.setAttribute('data-category', categoryId);
                 a.setAttribute('data-recipe-id', recipe.id);
